a7ec6ed81ec8ab1ee7443c96f55165c27e600b26
Jindal <u411281@global.ual.com>
2025-07-14 01:40:43 +0530

    fixing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Harshima_Singla_Portfolio/',
+  base: '/',
   plugins: [react()],
 })
